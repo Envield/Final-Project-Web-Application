2ec45a03d0c68a0d06bc2a60fe037f9c7ed17e3b
Naoval Aryo <119141622+Envield@users.noreply.github.com>
2024-11-23 15:02:04 +0700

    Update about.py

--- utility/about.py
+++ utility/about.py
@@ -1,64 +1,17 @@
 import streamlit as st
 
-# Tambahkan CSS untuk tata letak responsif dan penataan elemen
-st.markdown("""
-    <style>
-    .center-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        margin: 20px 0; /* Tambahkan margin untuk kenyamanan */
-    }
-    .center-content img {
-        display: block;
-        margin-left: 140px; 
-        margin-right: 0px; 
-        margin-bottom: 20px; 
-    }
-    .center-content h1 {
-        text-align: center;
-        margin-top: 10px;
-    }
-    .content-description {
-        text-align: justify;
-        margin: 0 10px; /* Margin untuk kenyamanan membaca */
-    }
-    @media only screen and (max-width: 768px) {
-        .center-content {
-            margin: 10px 0;
-        }
-        .content-description {
-            margin: 0 5px;
-        }
-    }
-    </style>
-""", unsafe_allow_html=True)
-
 def show_about_page():
     # Tampilan untuk logo dan judul dengan elemen Center
-    with st.container():
-    # Membuat div dengan CSS untuk menata posisi gambar dan judul di tengah
-        st.markdown('<div class="center-content">', unsafe_allow_html=True)
-        st.image("./src/logo_buah (2).png", width=200)
-        st.markdown('</div>', unsafe_allow_html=True)
-    
-    # Judul dengan tag h1 (sesuaikan CSS di atas)
-        st.markdown('<h1 style="text-align: center;">Freshty.id</h1>', unsafe_allow_html=True)
-    
-    # Menutup div container
-        st.markdown('</div>', unsafe_allow_html=True)
-
-    # Deskripsi aplikasi
+    col_center = st.columns([2.2, 2, 1]) 
+    with col_center[1]:
+        st.image("./src/logo_buah (2).png", width=200)  
+        st.title("Freshty.id")  
     st.markdown("""
-        <div class="content-description">
-            <span>
-            Freshty merupakan aplikasi yang dirancang untuk membantu konsumen dan petani dalam memantau kualitas buah secara praktis melalui analisis gambar. Dengan teknologi prediksi dan klasifikasi kualitas buah berbasis gambar, Freshty bertujuan untuk meningkatkan efisiensi supply chain dan membantu dalam proses pemilihan buah yang lebih cermat. Aplikasi ini tidak hanya memberikan manfaat bagi konsumen dalam kehidupan sehari-hari tetapi juga memberikan solusi cerdas bagi petani untuk meningkatkan perencanaan distribusi.  
-            Dengan Freshty, baik konsumen maupun petani dapat lebih percaya diri dalam mengambil keputusan terkait penyimpanan, konsumsi, dan distribusi buah. Solusi ini diharapkan mampu mendukung proses supply chain yang lebih baik, mengurangi kerugian akibat buah yang cepat membusuk, dan memastikan pengalaman terbaik bagi setiap pengguna. Mari bergabung bersama kami untuk meningkatkan kualitas dan efisiensi dalam dunia buah-buahan! 🍎🍇🍌
-            </span>
-        </div>
-    """, unsafe_allow_html=True)
+                <div style="text-align: justify;">
+                <span>
+                Freshty merupakan aplikasi yang dirancang untuk membantu konsumen dan petani dalam memantau kualitas buah secara praktis melalui analisis gambar. Dengan teknologi prediksi dan klasifikasi kualitas buah berbasis gambar, Freshty bertujuan untuk meningkatkan efisiensi supply chain dan membantu dalam proses pemilihan buah yang lebih cermat. Aplikasi ini tidak hanya memberikan manfaat bagi konsumen dalam kehidupan sehari-hari tetapi juga memberikan solusi cerdas bagi petani untuk meningkatkan perencanaan distribusi.  
+                Dengan Freshty, baik konsumen maupun petani dapat lebih percaya diri dalam mengambil keputusan terkait penyimpanan, konsumsi, dan distribusi buah. Solusi ini diharapkan mampu mendukung proses supply chain yang lebih baik, mengurangi kerugian akibat buah yang cepat membusuk, dan memastikan pengalaman terbaik bagi setiap pengguna. Mari bergabung bersama kami untuk meningkatkan kualitas dan efisiensi dalam dunia buah-buahan! 🍎🍇🍌</span>
+                </div>""", unsafe_allow_html=True)
 
     st.divider()
     # Layout untuk bagian penjelasan dan gambar
